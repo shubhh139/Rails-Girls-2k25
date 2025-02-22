a0ba14a7f84e36c769bf35e2aa05ba05292492ea
Divya <tambat.divya06@gmail.com>
2025-02-22 18:33:58 +0530

    changes added

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :items
   resources :requests
   resources :users
-  root to: "home#index"
+  root to: "users/sessions#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
